eebbbda3fb0d69dcb62605221ac6def7242026b4
albertsanso@gmail.com <albertsanso@gmail.com>
2025-12-30 22:19:44 +0100

    fixes

--- tt-data-league-api-rest/src/main/java/org/cttelsamicsterrassa/data/api/rest/season_player_result/SeasonPlayerResultDto.java
+++ tt-data-league-api-rest/src/main/java/org/cttelsamicsterrassa/data/api/rest/season_player_result/SeasonPlayerResultDto.java
@@ -1,4 +1,16 @@
 package org.cttelsamicsterrassa.data.api.rest.season_player_result;
 
-public record SeasonPlayerResultDto() {
+import org.cttelsamicsterrassa.data.core.domain.model.CompetitionInfo;
+import org.cttelsamicsterrassa.data.core.domain.model.SeasonPlayer;
+
+public record SeasonPlayerResultDto(
+        CompetitionInfo competitionInfo,
+        SeasonPlayer seasonPlayer,
+        String matchDay,
+        int matchDayNumber,
+        String matchGamePoints,
+        int matchGamesWon,
+        String matchLinkageId,
+        String matchPlayerLetter
+) {
 }
