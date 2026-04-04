40547d28e2aee01ad3bea4e77ebefc42e6c84c59
albertsanso@gmail.com <albertsanso@gmail.com>
2026-04-04 17:03:47 +0200

    fixes

--- tt-data-league-api-runtime/src/main/java/org/cttelsamicsterrassa/data/api/runtime/config/security/Users.java
+++ tt-data-league-api-runtime/src/main/java/org/cttelsamicsterrassa/data/api/runtime/config/security/Users.java
@@ -1,46 +1,35 @@
 package org.cttelsamicsterrassa.data.api.runtime.config.security;
 
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Users {
+    
     @Id
-    private int id;
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@JsonIgnore
+    @JsonProperty("id")
+    private Long id;
+    
+    @Column(name = "username", nullable = false, unique = true, length = 255)
+    @JsonProperty("username")
     private String username;
+    
+    @Column(name = "password", nullable = false, length = 255)
+    @JsonProperty("password")
     private String password;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Users{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
